ba7c4909322d8307e78aea4431b1c64d0ba488c1
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-21 01:06:35 +0200

    up

--- dwm-install-script.sh
+++ dwm-install-script.sh
@@ -228,6 +228,8 @@ gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font'
 ln -sf ~/.config/gtk-3.0/settings.ini ~/.config/gtk-4.0/settings.ini
 
 # Ustawienie konfiguracji programów root
+
+sudo mkdir -p /root/.config/
 sudo ln -sf ~/dotfiles/gtkrc-2.0/.gtkrc-2.0 /root/.gtkrc-2.0
 sudo ln -sf ~/dotfiles/vim/.vimrc /root/.vimrc
 sudo ln -sf ~/dotfiles/vim/.viminfo /root/.viminfo
