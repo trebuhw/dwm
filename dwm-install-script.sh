7cea5017175cf1162c2cfcce69f6bf44eaa08ba7
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-20 23:35:44 +0200

    up

--- dwm-install-script.sh
+++ dwm-install-script.sh
@@ -38,7 +38,7 @@ if [ "$DISTRO" = "arch" ]; then
     check_success "Nie udało się zainstalować podstawowych zależności"
     
     log "Instalacja dodatkowych pakietów..."
-    sudo pacman -S --needed --noconfirm zoxide git sxiv nwg-look lxappearance nitroogen neovim vim nsxiv fish fastfetch kitty stow starship trash-cli sxhkd nitrogen picom dunst polkit-gnome numlockx network-manager-applet parcellite feh
+    sudo pacman -S --needed --noconfirm eza zoxide git sxiv nwg-look lxappearance nitroogen neovim vim nsxiv fish fastfetch kitty stow starship trash-cli sxhkd nitrogen picom dunst polkit-gnome numlockx network-manager-applet parcellite feh
     check_success "Nie udało się zainstalować dodatkowych pakietów"
 elif [ "$DISTRO" = "ubuntu" ]; then
     log "Instalacja zależności DWM dla Ubuntu..."
@@ -49,7 +49,7 @@ elif [ "$DISTRO" = "ubuntu" ]; then
     check_success "Nie udało się zainstalować podstawowych zależności"
     
     log "Instalacja dodatkowych pakietów..."
-    sudo apt install -y git zoxide neovim vim sxiv fish nwg-look lxappearance nitrogen fastfetch kitty nsxiv stow starship trash-cli sxhkd nitrogen picom dunst policykit-1-gnome numlockx network-manager-gnome parcellite feh neofetch
+    sudo apt install -y git eza zoxide neovim vim sxiv fish nwg-look lxappearance nitrogen fastfetch kitty nsxiv stow starship trash-cli sxhkd nitrogen picom dunst policykit-1-gnome numlockx network-manager-gnome parcellite feh neofetch
     check_success "Nie udało się zainstalować dodatkowych pakietów"
 fi
 
