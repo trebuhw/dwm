beb559490ab8692ddc16b50cbacee9fdf4591e9c
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-25 00:36:57 +0200

    up install

--- dwm-install-script.sh
+++ dwm-install-script.sh
@@ -220,11 +220,12 @@ log "Aby uruchomić DWM, wyloguj się i wybierz sesję DWM z menedżera logowani
 sudo fc-cache -fv
 
 # Ustawienie theme gtk
-gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice' 
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+gsettings set org.gnome.desktop.interface cursor-size 20 
 gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "Catppuccin-Dark"
 gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-dracula-dark"
-gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font'
+gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 10'
 ln -sf ~/.config/gtk-3.0/settings.ini ~/.config/gtk-4.0/settings.ini
 
 # Ustawienie konfiguracji programów root
