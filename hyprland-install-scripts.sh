8c66d2d979ea6116ed933cd7fdd1f0515dccbfeb
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-26 17:32:31 +0200

    up install-v2 add hyprland scripts

--- hyprland-install-scripts.sh
+++ hyprland-install-scripts.sh
@@ -68,24 +68,20 @@ install_dwm_deps() {
     case "$DISTRO" in
         arch)
             log "Instalacja zależności DWM dla Arch Linux..."
-            sudo pacman -S --needed --noconfirm base-devel libx11 libxinerama libxft xorg xorg-server xorg-xinit
+            sudo pacman -S --needed --noconfirm brightnessctl grim hyprland mako networkmanager qt5-wayland qt6-wayland slurp swaybg swayidle swaylock waybar wireplumber wl-clipboard xdg-desktop-portal-hyprland 
             ;;
         ubuntu|debian)
             log "Instalacja zależności DWM dla $DISTRO..."
             sudo apt update
-            sudo apt install -y libharfbuzz-dev libxft-dev libpango1.0-dev build-essential \
-                libx11-dev libxinerama-dev libxcb1-dev libxcb-keysyms1-dev libxcb-icccm4-dev \
-                libx11-xcb-dev libxcb-util0-dev libxcb-randr0-dev suckless-tools libfreetype6-dev
+            sudo apt install -y 
             ;;
         fedora)
             log "Instalacja zależności DWM dla Fedora..."
-            sudo dnf install -y @development-tools libX11-devel libXinerama-devel libXft-devel \
-                xorg-x11-server-Xorg xorg-x11-xinit
+            sudo dnf install -y 
             ;;
         opensuse)
             log "Instalacja zależności DWM dla openSUSE..."
-            sudo sudo zypper --non-interactive install --no-recommends gcc harfbuzz-devel patterns-devel-base-devel_basis libX11-devel libXinerama-devel libXft-devel make \
-                xorg-x11-server xinit
+            sudo sudo zypper --non-interactive install --no-recommends 
             ;;
     esac
     check_success "Nie udało się zainstalować zależności DWM"
@@ -93,26 +89,35 @@ install_dwm_deps() {
 
 # Pakiety wspólne (repozytoria oficjalne)
 COMMON_PACKAGES=(
-    bash-completion bat blueman brightnessctl btop cups curl dunst feh file-roller firefox fish fzf galculator gcc gcolor3 gnome-disk-utility gparted gsettings-desktop-schemas gzip htop i3lock kitty mako meld neovim numlockx p7zip parcellite pavucontrol pdfarranger picom rclone ripgrep rofi rsync scrot stow sxhkd thunar thunar-archive-plugin thunar-volman time trash-cli tree tumbler unrar unzip vim vlc wget xclip xdg-user-dirs xfce4-notifyd zathura zoxide
+    azote bash-completion bat blueman btop brightnessctl cliphist cronie cups curl file-roller firefox fish floorp-bin foot fzf galculator gcc gcolor3 gnome-disk-utility gparted grimblast-git grub-customizer gzip htop hyprpicker i3lock iptables kitty mako mc meld neovim numlockx nwg-look os-prober pamixer parcellite pavucontrol pdfarranger picom polkit-gnome qemu-full qemu-img qt5ct ranger rclone ripgrep rofi rofi-emoji rsync sane sddm scrot simple-scan slurp starship stow sxhkd swappy swaybg swaylock-effects-git swaylock-fancy-git system-config-printer tela-circle-icon-theme-all thunar thunar-archive-plugin thunar-volman time timeshift trash-cli tree tumbler unrar unzip virt-install virt-manager virt-viewer vlc waybar wget wl-clipboard wlogout wofi xfce4-notifyd xfce4-terminal xclip xdg-user-dirs xreader yazi yt-dlp zathura zathura-pdf-mupdf zoxide
 )
 
 # Odpowiedniki i pakiety dodatkowe
 case "$DISTRO" in
     arch)
-        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty code eza fastfetch font-manager libreoffice-fresh libreoffice-fresh-pl polkit-gnome network-manager-applet nsxiv mlocate os-prober starship tldr xf86-input-synaptics xf86-video-intel wezterm yazi)
-        YAY_PACKAGES=(google-chrome lm_sensors nwg-look ueberzug waypaper)
+        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty code eza fastfetch font-manager libreoffice-fresh libreoffice-fresh-pl lm_sensors mousepad network-manager-applet nsxiv nwg-look mlocate os-prober polkit-gnome starship swappy tealdeer wezterm wofi yazi)
+        YAY_PACKAGES=(
+        bibata-cursor-theme brother-dcp1610w brscan4 github-desktop google-chrome hyprland-qtutils-git hyprutils-git lpm moc-pulse mkinitcpio-firmware ookla-speedtest-bin simple-sddm-theme-git sublime-text-4 waypaper wlogout
         ;;
     ubuntu)
-        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty eza fastfetch font-manager nwg-look policykit-1-gnome network-manager-gnome mlocate starship sxiv xserver-xorg-input-synaptics xserver-xorg-video-intel wezterm yazi)
+        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" 
+            
+            )
         ;;
     debian)
-        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty font-manager neofetch lxappearance policykit-1-gnome network-manager-gnome mlocate xserver-xorg-input-synaptics xserver-xorg-video-intel)
+        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}"
+
+            )
         ;;
     fedora)
-        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty fastfetch lxappearance polkit network-manager-applet sxiv xorg-x11-drv-synaptics xorg-x11-drv-intel )
+        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}"
+            
+            )
         ;;
     opensuse)
-        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}" alacritty eza fastfetch font-manager lxappearance opi polkit-gnome NetworkManager-applet mlocate starship sxiv sensors ueberzugpp wezterm yazi zathura-plugin-pdf-mupdf)
+        PACMAN_PACKAGES=("${COMMON_PACKAGES[@]}"
+
+            )
         ;;
 esac
 
@@ -346,44 +351,13 @@ log "Tworzenie kopii zapasowych plików konfiguracyjnych..."
 # Stow
 log "Tworzenie symlinków za pomocą stow..."
 cd ~/.dotfiles || { error "Nie można przejść do katalogu ~/.dotfiles"; exit 1; }
-stow Xresources/ alacritty/ background/ bash/ btop/ dunst/ fish/ fonts/ gtk-2.0/ gtk-3.0/ gtk-4.0/ gtkrc-2.0/ icons/ kitty/ mc/ nvim/ ranger/ rofi/ suckless/ nsxiv/ parcellite/ themes/ sxiv/ vim/ xinitrc/ xprofile/ yazi/ wezterm/ zathura/
+stow Xresources/ alacritty/ background/ bash/ btop/ dunst/ fish/ foot/ fonts/ gtk-2.0/ gtk-3.0/ gtk-4.0/ gtkrc-2.0/ hypr/ icons/ kitty/ mako/ mc/ mousepad/ nsxiv/ nvim/ qt5ct/ profile/ ranger/ rofi/ sublime-text/ swappy/ swaylock/ tealdeer/ themes/ thunar/ vim/ vscode/ waybar/ wezterm/ wlogout/ wofi/ wofifull/ xfce4/ yazi/ zathura/
 check_success "Błąd podczas wykonywania stow"
 
-# Kompilacja i instalacja DWM
-log "Kompilacja i instalacja DWM..."
-cd ~/.config/suckless/dwm || { error "Nie można przejść do katalogu DWM"; exit 1; }
-[ -f config.h ] && rm config.h
-sudo make && sudo make clean install && rm -f config.h
-check_success "Błąd podczas kompilacji DWM"
-
-# Kompilacja i instalacja DMENU
-log "Kompilacja i instalacja DMENU..."
-cd ~/.config/suckless/dmenu || { error "Nie można przejść do katalogu DMENU"; exit 1; }
-[ -f config.h ] && rm config.h
-sudo make && sudo make clean install && rm -f config.h
-check_success "Błąd podczas kompilacji DMENU"
-
-# Kompilacja i instalacja slstatus
-log "Kompilacja i instalacja slstatus..."
-cd ~/.config/suckless/slstatus || { error "Nie można przejść do katalogu slstatus"; exit 1; }
-[ -f config.h ] && rm config.h
-sudo make && sudo make clean install && rm -f config.h
-check_success "Błąd podczas kompilacji slstatus"
-
-# Kompilacja i instalacja st
-log "Kompilacja i instalacja st (terminal)..."
-cd ~/.config/suckless/st || { error "Nie można przejść do katalogu st"; exit 1; }
-sudo make && sudo make clean install
-check_success "Błąd podczas kompilacji st"
-
-# Instalacja pliku .desktop
-log "Kopiowanie pliku .desktop..."
-[ -d /usr/share/xsessions ] || sudo mkdir -p /usr/share/xsessions
-sudo cp ~/.config/suckless/usr/share/xsessions/dwm.desktop /usr/share/xsessions/
-check_success "Nie udało się skopiować pliku .desktop"
-
-log "Instalacja zakończona pomyślnie!"
-log "Aby uruchomić DWM, wyloguj się i wybierz sesję DWM z menedżera logowania."
+# SDDM
+[ -d ~/etc/sddm.conf.d ] && sudo mv /etc/sddm.conf.d/ /etc/sddm.conf.d.bak
+sudo ln -sf ~/.dotfiles/etc/.config/sddm.conf.d /etc/sddm.conf.d
+sudo ln -sf ~/.dotfiles/usr/share/sddm/themes/simple-sddm/ /usr/share/sddm/themes/simple-sddm/
 
 # Dodanie czcionek
 sudo fc-cache -fv
@@ -398,6 +372,7 @@ gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 10'
 ln -sf ~/.config/gtk-3.0/settings.ini ~/.config/gtk-4.0/settings.ini
 
 # Ustawienie konfiguracji programów root
+[ -d /root/.config ] && sudo mv /root/.config /root/.config.bak
 sudo mkdir -p /root/.config/
 sudo ln -sf ~/dotfiles/gtkrc-2.0/.gtkrc-2.0 /root/.gtkrc-2.0
 sudo ln -sf ~/dotfiles/vim/.vimrc /root/.vimrc
