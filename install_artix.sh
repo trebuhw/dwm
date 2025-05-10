e287a9428e08044a3a25ec8af3e9880da66d7d5e
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-05-11 01:33:00 +0200

    up artix

--- install_artix.sh
+++ install_artix.sh
@@ -176,7 +176,7 @@ gsettings set org.gnome.desktop.interface cursor-size 20
 gsettings set org.gnome.desktop.interface gtk-theme "Catppuccin-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "Catppuccin-Dark"
 gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-dracula-dark"
-gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font' 
+gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 10' 
 ln -sf ~/.config/gtk-3.0/settings.ini ~/.config/gtk-4.0/settings.ini
 
 # Wykonanie konfiguracji specyficznych dla Artix
