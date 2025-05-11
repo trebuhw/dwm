2c087f241aa7b13a0143f91d59f576d75b214f6c
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-05-11 02:24:38 +0200

    up artix

--- install_artix.sh
+++ install_artix.sh
@@ -60,7 +60,7 @@ install_dwm_deps() {
 
 # Pakiety dla Artix
 PACMAN_PACKAGES=(
-    alacritty bash-completion blueman brightnessctl btop curl dunst fastfetch feh file-roller firefox font-manager fzf galculator gcc gnome-disk-utility gparted gsettings-desktop-schemas gzip htop i3lock libreoffice-fresh libreoffice-fresh-pl meld neovim networkmanager networkmanager-runit network-manager-applet nsxiv numlockx p7zip pavucontrol picom plocate polkit-gnome ripgrep rofi rsync scrot sddm stow sxhkd thunar thunar-archive-plugin thunar-volman time tldr tlp qt5ct tree tumbler unrar unzip vim vlc wezterm wget xclip xdg-user-dirs xfce4-notifyd yazi zathura 
+    alacritty artix-archlinux-support bash-completion blueman brightnessctl btop curl dunst fastfetch feh file-roller firefox font-manager fzf galculator gcc gnome-disk-utility gparted gsettings-desktop-schemas gzip htop i3lock libreoffice-fresh libreoffice-fresh-pl meld neovim networkmanager networkmanager-runit network-manager-applet nsxiv numlockx p7zip pavucontrol picom plocate polkit-gnome ripgrep rofi rsync scrot sddm stow sxhkd thunar thunar-archive-plugin thunar-volman time tldr tlp qt5ct tree tumbler unrar unzip vim vlc wezterm wget xclip xdg-user-dirs xfce4-notifyd yazi zathura 
     )
 YAY_PACKAGES=(
     eza google-chrome lm_sensors nwg-look sublime-text-4 trash-cli zoxide
