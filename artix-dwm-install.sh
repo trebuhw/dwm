9bb78f96bcb197c634ea207335db13b15d5e0e6b
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-05-12 00:15:06 +0200

    add lightdm config

--- artix-dwm-install.sh
+++ artix-dwm-install.sh
@@ -102,7 +102,7 @@ artix_specific_configs() {
     # fonts
     sudo ln -sfv $HOME/.dotfiles/fonts/.fonts/JetBrainsMono /usr/share/fonts
     # config file
-    sudo ln -sfv $HOME/.dotfiles/etc/.config/lightdm/lightdm-gtk-greeter.conf/ /etc/lightdm
+    sudo ln -sfv $HOME/.dotfiles/etc/.config/lightdm/lightdm-gtk-greeter.conf /etc/lightdm
 }
 
 # Wykonywanie głównego kodu skryptu
